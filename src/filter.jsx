551248998ab6e141d1b16398ebed6c871a8f2494
Tomato <lilliput@Tomatos-MacBook-Air.local>
2022-09-26 17:20:52 +0530

    filter edits

--- src/filter.jsx
+++ src/filter.jsx
@@ -7,8 +7,8 @@ function Filter() {
         <select className="border-solid border-2 p-2 text-gray-500">
           <option value="Default sorting">Default sorting </option>
           <option value="sort by popularity">sort by popularity</option>
-          <option value="sort by avrage rating">sort by avrage rating</option>
-          <option value="short by latest">short by latest </option>
+          <option value="sort by avrage rating">sort by average rating</option>
+          <option value="sort by latest">sort by latest </option>
         </select>
       </div>
     </>
